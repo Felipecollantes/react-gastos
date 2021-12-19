bebade7ac083eb0d6f25b8558876e9668bdb5376
Felipe Pita Da Veiga Collantes <felipe.pcollantes@gmail.com>
2021-12-19 19:19:49 +0100

    [FPC] Configurado para gastos

--- src/infrastructure/components/App.tsx
+++ src/infrastructure/components/App.tsx
@@ -1,25 +1,23 @@
-import { FC, useState } from "react"
-import { List } from "../../domain/models/List"
-import { Outlay } from "../../domain/models/Outlay"
-import { listService } from "../../domain/services/List.service"
-import { OutlayList } from "./OutlayList"
+import { FC, useState } from 'react'
+import { List } from '../../domain/models/List'
+import { Outlay } from '../../domain/models/Outlay'
+import { listService } from '../../domain/services/List.service'
+import { OutlayList } from './OutlayList'
 
 type AppProps = {
   msg: string
 }
 
 const App: FC<AppProps> = ({ msg }) => {
-  const [list, setList] = useState<List | undefined>(undefined)
+  // const [list, setList] = useState<List | undefined>(undefined)
 
-  const handleAddToCart = (outlay: Outlay) => {
-    setList(listService.addOutlayToList(outlay, list))
-  }
+  // const handleAddToCart = (outlay: Outlay) => {
+  //   setList(listService.addOutlayToList(outlay, list))
+  // }
 
   return (
     <div className="App">
-      <h1>{msg}</h1>
-      <OutlayList onSelectOutlay={handleAddToCart} />
-      {list && <p>Items on list: {list.items.length}</p>}
+      <OutlayList />
     </div>
   )
 }
