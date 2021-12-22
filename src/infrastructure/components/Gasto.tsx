3e739f583cf69edbbe0ed68bfe09768b3ca34b6e
Felipe Pita Da Veiga Collantes <felipe.pcollantes@gmail.com>
2021-12-22 08:04:16 +0100

    [FPC] Clean className

--- src/infrastructure/components/Gasto.tsx
+++ src/infrastructure/components/Gasto.tsx
@@ -3,26 +3,24 @@ import styles from './list.module.css'
 
 export const Gasto = ({ outlay, setOutlays }: any) => {
   return (
-    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
-      <li key={outlay.id} className={styles.item}>
-        <ul className={styles.center}>
-          <li>
-            <b>Persona: </b> {outlay.namePerson}
-          </li>
-          <li>
-            <b>Motivo: </b>
-            {outlay.nameOutlay}
-          </li>
-          <li>
-            <b>Gasto: </b>
-            {outlay.price}
-          </li>
-          <li>
-            <b>Fecha: </b>
-            {outlay.since}
-          </li>
-        </ul>
-      </li>
-    </div>
+    <li key={outlay.id} className={styles.item}>
+      <ul className={styles.center}>
+        <li>
+          <b>Persona: </b> {outlay.namePerson}
+        </li>
+        <li>
+          <b>Motivo: </b>
+          {outlay.nameOutlay}
+        </li>
+        <li>
+          <b>Gasto: </b>
+          {outlay.price}
+        </li>
+        <li>
+          <b>Fecha: </b>
+          {outlay.since}
+        </li>
+      </ul>
+    </li>
   )
 }
