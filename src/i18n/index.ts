96df0ea99d55f0d128bcc49e1639cb11b8c243bd
Felipe Pita Da Veiga Collantes <felipe.pcollantes@gmail.com>
2021-12-22 13:16:05 +0100

    [FPC] i18n created

---
+++ src/i18n/index.ts
@@ -0,0 +1,18 @@
+const translate =  {
+    FORM:{
+        TITLE: 'Agregar Gasto',
+        INPUT_PERSON:'Nombre de la persona',
+        INPUT_OUTLAY:'Motivo del gasto',
+        INPUT_AMOUNT:'Cantidad',
+        BUTTON: 'Agregar'
+    },
+    LIST:{
+        TITLE: 'Lista Gastos',
+        PERSON:'Persona',
+        REASON:'Motivo',
+        OUTLAY:'Gasto',
+        DATE:'Fecha',
+    }
+}
+
+export default translate
